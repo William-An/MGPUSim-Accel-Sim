c4ebfc8043cb194abd88d851530ef031db6b3721
Weili An <an107@purdue.edu>
2022-06-08 14:21:26 -0400

    augment script to move all json files in a folder

--- samples/collect_stats.sh
+++ samples/collect_stats.sh
@@ -1,6 +1,16 @@
 #!/usr/bin/env bash
+# Create stats dir
+statsDir=$(readlink -f ./stats/)  # absolute path to stats folder
+if [[ ! -e $statsDir ]]; then
+  mkdir $statsDir;
+fi
+
 for dir in */; do 
+    if [[ "$dir" == "runner/" || "$dir" == "traces/" || "$dir" == "server/" || "$dir" == "stats/" ]]; then
+        continue
+    fi
     cd $dir; 
     ../collect_stats.py > ${dir%/}.json; 
+    cp ${dir%/}.json $statsDir;
     cd ..; 
 done
